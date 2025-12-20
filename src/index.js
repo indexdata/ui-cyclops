9fff8c7363df8d0b6eb63fa806d01c1fe6b35c60
Mike Taylor <mike@indexdata.com>
2025-12-20 13:38:44 +0000

    Remove unused PropTypes import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 import React from 'react';
-import PropTypes from 'prop-types';
 import Switch from 'react-router-dom/Switch';
 import Route from 'react-router-dom/Route';
 import SpectresRoute from './routes/SpectresRoute';
