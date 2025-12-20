1f148bc592418be3ab74af47ac8fc2df7456029f
Mike Taylor <mike@indexdata.com>
2025-12-20 13:08:15 +0000

    Make Spectres route component functional

--- src/routes/Spectres.js
+++ src/routes/Spectres.js
@@ -1,43 +1,36 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { stripesConnect } from '@folio/stripes/core';
 import { Pane, Paneset } from '@folio/stripes/components';
 import NewAppGreeting from '../components/new-app-greeting';
 
-class Spectres extends React.Component {
-  static propTypes = {
-    match: PropTypes.object.isRequired,
-  }
-
-  render() {
-    return (
-      <Paneset>
-        <Pane defaultWidth="fill" fluidContentWidth paneTitle="ui-cyclops">
-          <NewAppGreeting />
-          <br />
-          <ul>
-            <li data-test-application-examples>
-              View the
-              {' '}
-              <Link to={`${this.props.match.path}/examples`}>examples page</Link>
-              {' '}
-              to see some useful components.
-            </li>
-            <li data-test-application-guide>
-              Please refer to the
-              {' '}
-              <a href="https://github.com/folio-org/stripes/blob/master/doc/dev-guide.md">
-                Stripes Module Developer&apos;s Guide
-              </a>
-              {' '}
-              for more information.
-            </li>
-          </ul>
-        </Pane>
-      </Paneset>
-    );
-  }
+function Spectres(props) {
+  return (
+    <Paneset>
+      <Pane defaultWidth="fill" fluidContentWidth paneTitle="ui-cyclops">
+        <NewAppGreeting />
+        <br />
+        <ul>
+          <li data-test-application-examples>
+            View the
+            {' '}
+            <Link to={`${props.match.path}/examples`}>examples page</Link>
+            {' '}
+            to see some useful components.
+          </li>
+          <li data-test-application-guide>
+            Please refer to the
+            {' '}
+            <a href="https://github.com/folio-org/stripes/blob/master/doc/dev-guide.md">
+              Stripes Module Developer&apos;s Guide
+            </a>
+            {' '}
+            for more information.
+          </li>
+        </ul>
+      </Pane>
+    </Paneset>
+  );
 }
 
 export default stripesConnect(Spectres);
