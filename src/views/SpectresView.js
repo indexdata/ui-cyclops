6b4d030b967bff490bdbd3dad7032120fbc76559
Mike Taylor <mike@indexdata.com>
2026-02-03 18:26:19 +0000

    Basic MCL

--- src/views/SpectresView.js
+++ src/views/SpectresView.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import { Pane, Paneset,  Headline, Icon } from '@folio/stripes/components';
+import { Pane, Paneset,  Headline, Icon, MultiColumnList } from '@folio/stripes/components';
 
 
 
 function renderList(spectres) {
-  return <pre>{JSON.stringify(spectres, null, 2)}</pre>
+  const contentData = spectres.data.map(row => ({
+    id: row.values[0],
+    author: row.values[1],
+    title: row.values[2],
+    full_vendor_name: row.values[3],
+    availability: row.values[4],
+  }));
+
+  return (
+    <>
+      <div />{/* For some reason, if we omit this the MCL does not render */}
+      <MultiColumnList
+        contentData={contentData}
+      />
+      <pre>{JSON.stringify(spectres, null, 2)}</pre>
+    </>
+  );
 }
 
 
