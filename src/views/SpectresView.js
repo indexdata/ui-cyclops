1118d888f658afdb6fc65a6a4adf797eb494e130
Mike Taylor <mike@indexdata.com>
2026-02-03 18:17:43 +0000

    Separate out list-renderer

--- src/views/SpectresView.js
+++ src/views/SpectresView.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { Pane, Paneset,  Headline, Icon } from '@folio/stripes/components';
 
 
+
+function renderList(spectres) {
+  return <pre>{JSON.stringify(spectres, null, 2)}</pre>
+}
+
+
 export default function SpectresView({ loaded, spectres }) {
   return (
     <Paneset static>
@@ -13,7 +19,7 @@ export default function SpectresView({ loaded, spectres }) {
           {loaded && <>{spectres.data.length} objects</>}
         </Headline>
         {loaded
-          ? <pre>{JSON.stringify(spectres, null, 2)}</pre>
+          ? renderList(spectres)
           : <Icon icon="spinner-ellipsis" />
         }
       </Pane>
