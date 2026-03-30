9a2997a7a45d491a152e3a304b2734a8d9079a47
Mike Taylor <mike@indexdata.com>
2026-03-30 20:36:45 +0100

    Attempt (failed) to fix navigating back to List

--- src/Tabs.js
+++ src/Tabs.js
@@ -8,13 +8,12 @@ import { useNav } from './NavContext';
 
 const segments = [{
   name: 'home',
-  needSelected: false,
 }, {
   name: 'project',
-  needSelected: true,
+  idField: 'id',
 }, {
   name: 'list',
-  needSelected: true,
+  idField: 'name',
 }];
 
 
@@ -28,16 +27,16 @@ function Tabs() {
       <div style={{ display: 'flex', justifyContent: 'center', marginTop: '0.5em' }}>
         <ButtonGroup>
           {
-            segments.map(({ name, needSelected }) => {
+            segments.map(({ name, idField }) => {
               const segmentNav = nav[name];
               const fullBase = '/' + base + '/';
               const effectiveTab = location.pathname.replace(fullBase, '').replace(/\/.*/, '');
               const selected = (effectiveTab === name);
-              const disabled = needSelected && !segmentNav?.name;
+              const disabled = !!idField && !segmentNav?.name;
               return (
                 <Button
                   key={`${name}`}
-                  to={`/${base}/${name}/${segmentNav?.id || ''}`}
+                  to={`/${base}/${name}/${segmentNav?.[idField] || ''}`}
                   buttonStyle={selected ? 'primary' : 'default'}
                   disabled={disabled}
                 >
