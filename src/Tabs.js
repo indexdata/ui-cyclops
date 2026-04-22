6c325dab97a10b96841b8b69c06fb4292059f571
Mike Taylor <mike@indexdata.com>
2026-04-22 09:43:40 +0100

    When generating tabs allow for different idField per tab

--- src/Tabs.js
+++ src/Tabs.js
@@ -8,13 +8,13 @@ import { useNav } from './NavContext';
 
 const segmentsConfig = [{
   name: 'home',
-  needIdField: false,
+  idField: undefined
 }, {
   name: 'project',
-  needIdField: true,
+  idField: 'altName'
 }, {
   name: 'list',
-  needIdField: true,
+  idField: 'name'
 }];
 
 
@@ -27,14 +27,14 @@ function Tabs() {
     <div style={{ display: 'flex', justifyContent: 'center', marginTop: '0.5em' }}>
       <ButtonGroup>
         {
-          segmentsConfig.map(({ name, needIdField }) => {
+          segmentsConfig.map(({ name, idField }) => {
             const segmentNav = nav[name];
             const fullBase = '/' + base + '/';
             const effectiveTab = location.pathname.replace(fullBase, '').replace(/\/.*/, '');
             const sl = segmentNav.location;
             const to = sl ? `${sl.pathname}${sl.search}` : `${packageInfo.stripes.route}/${name}`;
             const selected = (effectiveTab === name);
-            const disabled = needIdField && !segmentNav.name;
+            const disabled = idField && !segmentNav[idField];
             return (
               <Button
                 key={`${name}`}
@@ -44,7 +44,7 @@ function Tabs() {
               >
                 <FormattedMessage
                   id={`ui-cyclops.tab.${name}`}
-                  values={{ name: segmentNav.name }}
+                  values={{ name: segmentNav[idField] }}
                 />
               </Button>
             );
